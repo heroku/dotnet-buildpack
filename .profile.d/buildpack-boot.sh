2607041c51642dead4770152d878872e098b1c50
Michael <friism@gmail.com>
2014-12-19 12:54:20 -0800

    remove xsp

--- .profile.d/buildpack-boot.sh
+++ .profile.d/buildpack-boot.sh
@@ -1,2 +1,2 @@
-export PATH="$PATH:/app/mono/bin:/app/xsp/bin:/usr/local/bin:/usr/bin:/bin"
+export PATH="$PATH:/app/mono/bin:/usr/local/bin:/usr/bin:/bin"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/app/mono/lib"
