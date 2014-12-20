5c9e071bd65e003396d3bf5b4c3146ea5df595bc
Michael <friism@gmail.com>
2014-12-19 18:32:27 -0800

    add libuv to lib path

--- .profile.d/buildpack-boot.sh
+++ .profile.d/buildpack-boot.sh
@@ -1,2 +1,2 @@
 export PATH="$PATH:/app/mono/bin:/usr/local/bin:/usr/bin:/bin"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/app/mono/lib"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/app/mono/lib:/app/libuv/lib"
