d8d147a73e0ec9b7d3d0c52e093fb0c556f49c51
Michael <friism@gmail.com>
2014-12-20 09:15:36 -0800

    more

--- .profile.d/buildpack-boot.sh
+++ .profile.d/buildpack-boot.sh
@@ -1,2 +1,2 @@
-export PATH="$PATH:/app/mono/bin:/usr/local/bin:/usr/bin:/bin"
+export PATH="$PATH:/app/mono/bin:/usr/local/bin:/usr/bin:/bin:/app/kre/packages/default/bin"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/app/mono/lib:/app/libuv/lib"
