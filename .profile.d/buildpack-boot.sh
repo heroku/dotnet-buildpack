fae188326e22355e4ee0d2a0237255f21f63c663
Michael Friis <friism@gmail.com>
2015-05-10 13:21:26 -0700

    assorted improvements

--- .profile.d/buildpack-boot.sh
+++ .profile.d/buildpack-boot.sh
@@ -1,3 +1,4 @@
-export PATH="$PATH:/app/mono/bin:/usr/local/bin:/usr/bin:/bin:/app/kre/runtimes/default/bin"
+DNX_BIN=`find .dnx/runtimes -type d -name bin`
+export PATH="$PATH:/app/mono/bin:$DNX_BIN"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/app/mono/lib:/app/libuv/lib"
 export MONO_OPTIONS=--server
