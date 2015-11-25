ebd4356dd92be6dcaa36c72cc8ff396343a15a10
Michael Friis <friism@gmail.com>
2015-11-24 21:37:33 -0800

    get rid of mono

--- .profile.d/buildpack-boot.sh
+++ .profile.d/buildpack-boot.sh
@@ -1,4 +1,2 @@
 DNX_BIN=`find .dnx/runtimes -type d -name bin`
-export PATH="$PATH:/app/mono/bin:$DNX_BIN"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/app/mono/lib:/app/libuv/lib"
-export MONO_OPTIONS=--server
+export PATH="$PATH:$DNX_BIN"
